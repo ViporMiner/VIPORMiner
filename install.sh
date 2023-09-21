2b8e75bc5c81b08c82602549cceca84ead5c987d
TLSminerProxy <57853730+TLSminer@users.noreply.github.com>
2023-09-21 13:11:59 +0800

    3.6.0

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="3.6.0"
-DOWNLOAD_HOST="https://github.com/EvilGenius-dot/shortcut/raw/main/Readme/3/linux"
+DOWNLOAD_HOST="https://github.com/TLSminer/VIPORMiner/raw/main/linux"
 ORIGIN_EXEC="vipor-${VERSION}"
 
 SERVICE_NAME="viporservice"
