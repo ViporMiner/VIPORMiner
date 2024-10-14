a4cdd1946c1b9b9f89998e4878c5a19ab60b62dd
ViporMinerProxy <57853730+ViporMiner@users.noreply.github.com>
2024-10-14 23:24:59 +0800

    4.1.3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="4.1.2"
+VERSION="4.1.3"
 DOWNLOAD_HOST="https://github.com/VIPORMiner/VIPORMiner/raw/main/linux"
 ORIGIN_EXEC="vipor-${VERSION}"
 
@@ -390,6 +390,7 @@ ISSUE() {
     echo "4.1.0"
     echo "4.1.1"
     echo "4.1.2"
+    echo "4.1.3"
 }
 
 filterResult() {
